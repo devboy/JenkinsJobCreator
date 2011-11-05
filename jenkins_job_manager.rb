37fc31cbf39eb11096dd97cbfc9e613ccad97d4b
devboy <dominic.graefen@gmail.com>
2011-11-05 13:47:20 +0100

    now finds jobs to create and jobs to delete

--- jenkins_job_manager.rb
+++ jenkins_job_manager.rb
@@ -27,6 +27,10 @@ class JenkinsJobManager
     @job+"(#{branch.gsub("origin/", "").gsub(/[^a-zA-Z0-9_-]/, "-")})"
   end
 
+  def job_dir job
+    "#{@jenkins_dir}/jobs/#{job}"
+  end
+
   def candidates
     remote_branches.map { |b| {:job => job_name(b), :branch => b} }
   end
@@ -41,4 +45,28 @@ class JenkinsJobManager
         keep_if{ |j| job_candidates.detect{ |c| c[:job] == j }.nil? }
   end
 
+  class JobAction < Thor
+    include Thor::Actions
+
+    def self.source_paths
+      File.dirname __FILE__
+    end
+
+    def create_job_from_template job, job_dir, branch
+      @branch, @job = branch, job
+      directory "job_template", job_dir
+    end
+
+    no_tasks {
+
+      def branch
+        @branch
+      end
+
+      def job
+        @job
+      end
+    }
+
+  end
 end
